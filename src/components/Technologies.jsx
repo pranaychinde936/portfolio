510ecd9c33904b9de9afcadc44972b69121c0576
Pranay <pranaychinde@gmail.com>
2025-03-29 23:08:30 +0530

    complete animation

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,32 +1,95 @@
-import { FaGit, FaPython, FaGithub, FaDocker, FaAws, FaJs } from "react-icons/fa"
+import {
+  FaGit,
+  FaPython,
+  FaGithub,
+  FaDocker,
+  FaAws,
+  FaJs,
+} from "react-icons/fa";
 import { TbBrandCpp } from "react-icons/tb";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
-    <div className='border-b border-neutral-800 pb-24'>
-        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <TbBrandCpp className='text-7xl text-white-600' />
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaPython className='text-7xl text-blue-400' />
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaJs className='text-7xl text-yellow-400' />
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaAws className='text-7xl text-orange-400' />
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaDocker className='text-7xl text-blue-600' />
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaGithub className='text-7xl text-white-700' />
-            </div>
-        </div>
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandCpp className="text-7xl text-white-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaPython className="text-7xl text-blue-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaJs className="text-7xl text-yellow-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaAws className="text-7xl text-orange-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaDocker className="text-7xl text-blue-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaGithub className="text-7xl text-white-700" />
+        </motion.div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Technologies
+export default Technologies;
